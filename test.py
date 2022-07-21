8bbcd0fcc1f35b2a3b9841ad873f5ca1a4b200ea
John Carlson <johncarlson@pm.me>
2022-07-20 22:34:02 -0400

    Test out of order delete

--- test.py
+++ test.py
@@ -4,14 +4,15 @@ import discord
 
 
 class FakeChannel:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self, channel_id):
+        self.id = channel_id
 
 
 class FakeMessage:
-    content = ""
-    channel = FakeChannel(1)
-    created_at = 1
+    def __init__(self, content="", timestamp=1, channel_id=1):
+        self.content = content
+        self.created_at = timestamp
+        self.channel = FakeChannel(channel_id)
 
 
 class UndeleteBotTest(unittest.IsolatedAsyncioTestCase):
@@ -30,6 +31,20 @@ class UndeleteBotTest(unittest.IsolatedAsyncioTestCase):
         time, value = self.bot.message_queues[1].get()
         self.assertEqual(time, 1)
         self.assertEqual(value.content, "Message 1")
+
+    async def test_out_of_order_delete_calls(self):
+        message1 = FakeMessage("1", 1)
+        message2 = FakeMessage("2", 2)
+        message3 = FakeMessage("3", 3)
+
+        await self.bot.on_message_delete(message2)
+        await self.bot.on_message_delete(message3)
+        await self.bot.on_message_delete(message1)
+
+        for i in range(1, 3):
+            time, value = self.bot.message_queues[1].get()
+            self.assertEqual(time, i)
+            self.assertEqual(value.content, str(i))
 
 
 if __name__ == '__main__':
